132c1793690c3ee7ebab871d19ab226d50668692
Adebayo Freeman <adebayofreeman4@gmail.com>
2024-08-08 08:15:52 +0100

    add treasure map

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,7 +1,23 @@
-import random
+# import random
 
-num = random.randint(0,1)
-if num == 0:
-    print("Tail")
-else:
-    print("Head")
+# num = random.randint(0,1)
+# if num == 0:
+#     print("Tail")
+# else:
+#     print("Head")
+
+row1 = ["⬜","⬜","⬜"]
+row2 = ["⬜","⬜","⬜"]
+row3 = ["⬜","⬜","⬜"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+vertical = int(position[0])
+horizontal = int(position[1]) 
+map[horizontal - 1][vertical -1] = "X"
+print(f"{row1}\n{row2}\n{row3}")
+
+
+# print(f"{vertical} \n{horizontal}")
